beef4d601639a568734a26c681c60fbc684af035
Remy349 <santoraga15@gmail.com>
2023-08-19 23:17:03 -0600

    Add post model and database support.

---
+++ backend/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config(object):
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.app")
---
+++ backend/flaskr/models/post.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from datetime import datetime
+from flaskr.extensions import db
+
+
+class PostModel(db.Model):
+    __tablename__ = "posts"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    title = sa.Column(sa.String(200), nullable=False, unique=False)
+    content = sa.Column(sa.Text, nullable=False, unique=False)
+    created_at = sa.Column(sa.DateTime, default=datetime.utcnow)
