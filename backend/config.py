399203c9d0b4631dc0228ca26caee1fc1ab8e588
Remy349 <santoraga15@gmail.com>
2023-08-20 01:06:33 -0600

    Add post resource and schemas for the REST API.

--- backend/config.py
+++ backend/config.py
@@ -10,9 +10,15 @@ class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
+    API_TITLE = "Blog REST API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
 
 
 class DevelopmentConfig(Config):
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.db")
 
 
---
+++ backend/flaskr/resources/post.py
@@ -0,0 +1,41 @@
+from flask_smorest import Blueprint
+from flask.views import MethodView
+from flaskr.controllers import PostController
+from flaskr.schemas import PostSchema, PostUpdateSchema
+
+bp = Blueprint("posts", __name__, description="Operations on posts")
+
+controller = PostController()
+
+
+@bp.route("/posts/<int:post_id>")
+class Post(MethodView):
+    @bp.response(200, PostSchema)
+    def get(self, post_id):
+        """ Get a single post by ID """
+        return controller.get_post(post_id)
+
+    @bp.arguments(PostUpdateSchema)
+    @bp.response(200, PostSchema)
+    def put(self, post_data, post_id):
+        """ Update a post by ID """
+        return controller.update_post(post_data, post_id)
+
+    @bp.response(204)
+    def delete(self, post_id):
+        """ Delete a post by ID """
+        return controller.delete_post(post_id)
+
+
+@bp.route("/posts")
+class PostList(MethodView):
+    @bp.response(200, PostSchema(many=True))
+    def get(self):
+        """ Get a list of all posts """
+        return controller.get_posts()
+
+    @bp.arguments(PostSchema)
+    @bp.response(201, PostSchema)
+    def post(self, post_data):
+        """ Create a new post """
+        return controller.create_post(post_data)
